b72310fcd6f14926419734aa337b66a3844adf3e
Ivan Villanueva <35877147+IvanVO@users.noreply.github.com>
2020-09-30 18:57:46 -0500

    Update lematizador.py

--- Activity 4/lematizador.py
+++ Activity 4/lematizador.py
@@ -5,10 +5,9 @@ stemmer_en = Stemmer.Stemmer('english')
 class Stemmer:
     def __init__(self):
         
-def main():
-    word = input("Insert a word: ")
-    print(stemmer_en.stemWord(word))
+    def main():
+        word = input("Insert a word: ")
+        print(stemmer_en.stemWord(word))
 
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
