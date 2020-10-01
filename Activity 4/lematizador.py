c0b1955cdc59446f92f12aa318241963079b70f6
Ivan Villanueva <35877147+IvanVO@users.noreply.github.com>
2020-10-01 13:15:09 -0500

    Add files via upload

--- Activity 4/lematizador.py
+++ Activity 4/lematizador.py
@@ -1,10 +1,26 @@
 import Stemmer
 
 stemmer_en = Stemmer.Stemmer('english')
-
+punctuation_marks = [".", ":", ";", ",", "?", '"', "'", "(", ")", "!"]
 def main():
-    word = input("Insert a word: ")
-    print(stemmer_en.stemWord(word))
+    f = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/Liquid Water on Mars.txt", "r")
+    w = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/testLematizador.txt", "w")
+
+    for line in f.readlines():
+        items = line.split()
+        for i in items:
+            if i == "—":
+                i = i.replace("—", '')
+            for p in punctuation_marks:
+                if i.find(p):
+                    i = i.replace(p, '')
+                elif i[0] == '"' or i[-1] == '"':
+                    i.replace('"', "")
+
+                    print(i)
+            word = stemmer_en.stemWord(i)
+            w.write(f"{word} ")
+
 
 
 if __name__ == '__main__':
