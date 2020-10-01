d22627c5e3d285d0a2afc6271bbda05d87e61d44
IvanVO <angel.villanuevaoz@udlap.mx>
2020-10-01 17:18:00 -0500

    updated

--- Activity 4/lematizador.py
+++ Activity 4/lematizador.py
@@ -1,27 +1,31 @@
 import Stemmer
 
 stemmer_en = Stemmer.Stemmer('english')
-punctuation_marks = [".", ":", ";", ",", "?", '"', "'", "(", ")", "!"]
-def main():
-    f = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/Liquid Water on Mars.txt", "r")
-    w = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/testLematizador.txt", "w")
+punctuation_marks = [".", ":", ";", ",", "?", '"', "'", "(", ")", "!", "-"]
+
+def stemm(f, w):
 
     for line in f.readlines():
         items = line.split()
         for i in items:
             if i == "—":
                 i = i.replace("—", '')
+            elif i == ".":
+                i = i.replace(".", '')
+
             for p in punctuation_marks:
                 if i.find(p):
                     i = i.replace(p, '')
-                elif i[0] == '"' or i[-1] == '"':
-                    i.replace('"', "")
+                    i = i.replace('"', "")
 
-                    print(i)
             word = stemmer_en.stemWord(i)
-            w.write(f"{word} ")
+            w.write(f"{word.lower()} ")
 
+def main():
+    f = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/Liquid Water on Mars.txt", "r")
+    w = open("/home/ivanvillanueva/Desktop/Stemmeing/Activity 4/testLematizador.txt", "w")
 
+    stemm(f, w)
 
 if __name__ == '__main__':
     main()
